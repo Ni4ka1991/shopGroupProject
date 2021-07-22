ec3a19b3715c45ef7d85c459d16f7301174895a9
Ni4ka <astahova_nica@mail.ru>
2021-07-22 19:52:05 +0300

    Add currency

---
+++ models/Currency.py
@@ -0,0 +1,22 @@
+
+class Currency( self,  id, code, nominal, rate ):
+    self.id = id
+    self.code = code
+    self.nominal = nominal
+    self.rate = rate
+
+    def __str__( self ):
+        return f"\n "\
+        f"id:      {self.id}\n"\
+        f"code:    {self.code}\n"\
+        f"nominal: {self.nominal}\n"\
+        f"rate:    {self.rate}"
+    
+    def __repr__( self ):
+        return self.__str__()   
+   
+
+
+class CurrencyService( self ):
+    
+    def getCurrencies( self ): 
