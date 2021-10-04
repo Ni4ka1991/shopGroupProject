2fcf0a65435860e01ad205ef182f5a5a05b7d99a
Ni4ka <astahova_nica@mail.ru>
2021-10-04 12:34:58 +0300

    Logic change. Start. CorrencyService

---
+++ test.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+
+from boot import *
+from models.OrderItem import *
+from os import system
+from services.TestDataService import *
+from services.CurrencyService import *
+from models.Money import *
+from models.Product import *
+from models.Currency import *
+from ui.index import *
+
+
+
+system ("clear")
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
