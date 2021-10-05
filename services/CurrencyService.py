f0a10c934e7bf1b405f0fe65611edcbf52484d58
Ni4ka <astahova_nica@mail.ru>
2021-10-05 10:56:54 +0300

    Work on xml response. CurrencyService

--- services/CurrencyService.py
+++ services/CurrencyService.py
@@ -16,16 +16,24 @@ class CurrencyService:
         print( "waiting for server response ..." )
         url = f"https://www.bnm.md/ru/official_exchange_rates?get_xml=1&date={today}"
         res = requests.get( url )
-        data = res.text
-        print( data )
         currencies = []
 
-#        if res.status_code == 200:
-#            data = res.json()
-#            print(data)
+        if res.status_code == 200:
+            data = res.text
+            tree = ET.parse( data )
+            root = tree.getroot()
+
+            for i in range( 0, 3 ):
+                currencies.append(\
+                        Currency(\
+                        (root.findall(f"./Valute/{doc_tree[0]}"))[i].text,\
+                        (root.findall(f"./Valute/{doc_tree[1]}"))[i].text,\
+                        (root.findall(f"./Valute/{doc_tree[2]}"))[i].text,\
+                        (root.findall(f"./Valute/{doc_tree[4]}"))[i].text)\
+                                )
               
  
-#        else:
-#            raise Exception( "Connection Error!" )
+        else:
+            raise Exception( "Connection Error!" )
 
         return currencies
